cb56ae43b8cba4151031f626931790de238daab5
Rungruoi <rungruoi99it@gmail.com>
2020-06-08 22:58:52 +0700

    upgrade theme admin metronic

---
+++ src/views/auth/password/email.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.auth')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Reset password</div>
+
+                <div class="card-body">
+        
+                    <form method="POST">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="email" class="col-md-2 col-form-label text-md-right">Email</label>
+
+                            <div class="col-md-9">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="" required autocomplete="email" autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Send
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
